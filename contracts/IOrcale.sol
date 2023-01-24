e19b14837fb6cbec20c393b390dbf84c0925c046
Dylan Falconer <me@falconerd.com>
2023-01-24 18:58:25 +1100

    Going too fast, need to slow down

--- contracts/IOrcale.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.17;
-
-interface IOracle {
-    function get_prices() external returns (uint, uint);
-    function get_balances(uint total_supply) external returns (uint, uint);
-}
